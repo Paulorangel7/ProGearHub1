2e007522ebd29d331b76f4f85adf88c5c2deec2f
Paulorangel7 <14984@ait.nsw.edu.au>
2025-05-14 15:18:14 +1000

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -3,18 +3,21 @@
 
 require_once 'config.php';
 
+$response_message = '';
+$error_message = '';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Field names according to the order.html form 
     $customer_name = filter_input(INPUT_POST, 'customer_name', FILTER_SANITIZE_STRING);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $product_name = filter_input(INPUT_POST, 'product_name', FILTER_SANITIZE_STRING);
+    $customer_email = filter_input(INPUT_POST, 'customer_email', FILTER_SANITIZE_EMAIL);
+    $product_id = filter_input(INPUT_POST, 'product_id', FILTER_SANITIZE_STRING);
     $quantity = filter_input(INPUT_POST, 'quantity', FILTER_SANITIZE_NUMBER_INT);
-    $price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+  
 
     // Basic validation of mandatory fields
     if (empty($customer_name) || empty($email) || empty($product_name) || !isset($quantity) || !isset($price)) {
-        die('Error: Customer name, email, product name, quantity and price are mandatory.');
+        die('Error: Customer name, email, product id, quantity and price are mandatory.');
     }
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -32,14 +35,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         // SQL query to insert the data into the ‘orders’ table 
         // total_price is generated automatically by the bank
-        $sql = "INSERT INTO orders (customer_name, email, product_name, quantity, price) 
-                VALUES (:customer_name, :email, :product_name, :quantity, :price)";
+        $sql = "INSERT INTO orders (customer_name, customer_email, product_id, quantity, price) 
+                VALUES (:customer_name, :customer_email, :product_id, :quantity, :price)";
         
         $stmt = $pdo->prepare($sql);
 
         $stmt->bindParam(':customer_name', $customer_name, PDO::PARAM_STR);
-        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->bindParam(':product_name', $product_name, PDO::PARAM_STR);
+        $stmt->bindParam(':customer_email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':product_id', $product_id, PDO::PARAM_STR);
         $stmt->bindParam(':quantity', $quantity, PDO::PARAM_INT);
         $stmt->bindParam(':price', $price, PDO::PARAM_STR); // PDO::PARAM_STR is safer for decimals, PDO handles the conversion.
         
